893d579c28527adad4ada13b0da2a56cca1f35c2
Dmitry <infags@mail.ru>
2021-05-14 23:32:53 +0300

    Add project

---
+++ src/main/java/ru/new_java_simple_project/simple_project/SimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.new_java_simple_project.simple_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/new_java_simple_project/simple_project/SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.new_java_simple_project.simple_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
